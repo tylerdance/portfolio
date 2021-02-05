a1809eccd3c7debd84c49b9000f2cafe54af4deb
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-02-05 10:19:20 -0500

    about section edit

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,8 @@ function About() {
             <div class="about">
                 <p>After finishing 13 weeks of full time web development at General Assembly last January, I’ve developed 3 fullstack applications using React,js, MongoDB, and Express, and one mini game using Javascript. The independent nature of the work was rewarding, but I ultimately got the most out of group projects. Now having a good understanding of what managing a project means as far as source control, task delegation, and merge conflicts, I’m looking forward to bringing this knowledge to my next software engineering role.</p>
                 <p>The majority of the immersive program was spent writing with Javascript, so while that is what this portfolio currently reflects, I look forward to including a Python project here soon to better represent my current skill set. Each of the four projects that is presented on this portfolio was developed over the course of a week or two, each project building on the last as my knowledge of Javascript grew. Given the short time frame for production, I did not have the time to finish everything I hoped to accomplish on each project. I am currently in the process of going back over each project to make them responsive to screen size, implementing features that were left as stretch goals, and refactoring code to be more concise and legible.</p>
-                <p>As proud as I am of this collection of apps, I am keen to start working on projects that are more pointed towards location based technologies. I’m currently researching different open source GIS technologies such as Leaflet and ArcGIS to see how I can incorporate these kinds of technologies into my next project and my recent Trail Hunter project.</p>
+                <p>As proud as I am of this collection of apps, I'm eager to start working on projects that are more pointed towards geospatial technologies. I’m currently researching different open source GIS technologies such as Leaflet and ArcGIS to see how I can incorporate these kinds of technologies into my next project and my recent Trail Hunter project.</p>
             </div>
-
         </div>
     )
 }
