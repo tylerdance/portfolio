0ec7502961842cdfca816606af6737e6e983a7f0
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-02-22 08:41:07 -0500

    lol report highlight image update

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ function About() {
             <div class="about">
                 <p>During a 13 week sprint of full time software development at General Assembly, I developed three fullstack applications using React.js, MongoDB, SQL, Express, and Node, and one mini game in Javascript. Exposure to such a fast paced environment reassured me of my potential to learn new technologies quickly and effectively while maintaining an appetite for even more challenging problems. The independent nature of the work was rewarding, but I learned more from the group projects with small production teams. Now having a good understanding of project management in terms of source control, merge conflicts, and time management, I’m looking forward to bringing these skills to my next software engineering role.</p>
                 <br />
-                <p>Currently, I am developing a ranked player analysis tool for League of Legends competitive play. Users are able to view current leaderboards sorted by League Points, and will soon be able to also sort by win/loss ratio. This app has a React.js front end and Express back end making the Axios requests to avoid CORS errors. Live site and repo links are below under "LoL Ranked Report".</p>
+                <p>Currently, I am developing a ranked player analysis tool for League of Legends competitive play. Users are able to view current leaderboards sorted by League Points, and will soon be able to also sort by win/loss ratio and search an individual player's match history. This app has a React.js front end and Express back end making the Axios requests to avoid CORS errors. Live site and repo links are below under "LoL Ranked Report".</p>
             </div>
 
         </div>
--- src/components/RecentWork.js
+++ src/components/RecentWork.js
@@ -6,64 +6,64 @@ function RecentWork() {
                 <h2 >Recent Work</h2>
 
             </div>
-            <div class="lol-div">
-                <div id="token-sub-div">
-                    <h3 ><a class="recents-header" href="http://lolrankedreport.herokuapp.com/" target="_blank" rel="noreferrer">LoL Ranked Report</a></h3>
-                    <p class="description">League of Legends competitive play analysis tool hosting current rankings for the top three tiers.</p>
-                    {/* <p class="description">Open an <em>incognito</em> window with a second account to demo the chat functionality.</p> */}
-                    {/* <br /> */}
-                    <p class="tech-stack"><strong>React.js, Express, Node.js</strong></p>
-                    <div class="links-div">
-                        <div><a class="projects-info" href="http://lolrankedreport.herokuapp.com/" target="_blank" rel="noreferrer">live site</a></div>
-                        <div><a class="projects-info" href="https://github.com/tylerdance/lol-report" target="_blank" rel="noreferrer">front end repo</a> | <a class="projects-info" href="https://github.com/tylerdance/lol-report-backend" target="_blank" rel="noreferrer">back end repo</a></div>
-                        <div></div>
+                <div class="lol-div">
+                    <div id="token-sub-div">
+                        <h3 ><a class="recents-header" href="http://lolrankedreport.herokuapp.com/" target="_blank" rel="noreferrer">LoL Ranked Report</a></h3>
+                        <p class="description">League of Legends competitive play analysis tool hosting current rankings for the top three tiers.</p>
+                        <p class="description">Individual player stat search currently in progress.</p>
+                    
+                        <p class="tech-stack"><strong>React.js, Express, Node.js</strong></p>
+                        <div class="links-div">
+                            <div><a class="projects-info" href="http://lolrankedreport.herokuapp.com/" target="_blank" rel="noreferrer">live site</a></div>
+                            <div><a class="projects-info" href="https://github.com/tylerdance/lol-report" target="_blank" rel="noreferrer">front end repo</a> | <a class="projects-info" href="https://github.com/tylerdance/lol-report-backend" target="_blank" rel="noreferrer">back end repo</a></div>
+                            <div></div>
 
+                        </div>
+                    </div>
+                    <div>
+                        <img className="ss" src="https://res.cloudinary.com/df3zbbtc1/image/upload/v1614000777/Screen_Shot_2021-02-22_at_8.31.18_AM_pqslyf.png" alt="" />
                     </div>
                 </div>
-                <div>
-                    <img className="ss" src="http://res.cloudinary.com/dok4pz3i3/image/upload/v1613838363/Screen_Shot_2021-02-20_at_11.24.38_AM_zctqgf.png" alt="" />
-                </div>
-            </div>
             
-            <div class="token-div">
-                <div id="token-sub-div">
-                    <h3 ><a class="recents-header" href="http://token-chat.herokuapp.com/" target="_blank" rel="noreferrer">Token</a></h3>
-                    <p class="description">Websocket based messaging app experiment where users can find connections based on their location. Swipe right to send a chat invitation, swipe left to shuffle the database.</p>
-                    {/* <p class="description">Open an <em>incognito</em> window with a second account to demo the chat functionality.</p> */}
-                    {/* <br /> */}
-                    <p class="tech-stack"><strong>React.js, MongoDB, Express, Node.js</strong></p>
-                    <div class="links-div">
-                        <div><a class="projects-info" href="http://token-chat.herokuapp.com/" target="_blank" rel="noreferrer">live site</a></div>
-                        <div><a class="projects-info" href="https://github.com/tylerdance/SEI-project4-frontend" target="_blank" rel="noreferrer">front end repo</a> | <a class="projects-info" href="https://github.com/tylerdance/SEI-project4-backend" target="_blank" rel="noreferrer">back end repo</a></div>
-                        <div></div>
+                <div class="token-div">
+                    <div id="token-sub-div">
+                        <h3 ><a class="recents-header" href="http://token-chat.herokuapp.com/" target="_blank" rel="noreferrer">Token</a></h3>
+                        <p class="description">Websocket based messaging app experiment where users can find connections based on their location. Swipe right to send a chat invitation, swipe left to shuffle the database.</p>
+                        {/* <p class="description">Open an <em>incognito</em> window with a second account to demo the chat functionality.</p> */}
+                        {/* <br /> */}
+                        <p class="tech-stack"><strong>React.js, MongoDB, Express, Node.js</strong></p>
+                        <div class="links-div">
+                            <div><a class="projects-info" href="http://token-chat.herokuapp.com/" target="_blank" rel="noreferrer">live site</a></div>
+                            <div><a class="projects-info" href="https://github.com/tylerdance/SEI-project4-frontend" target="_blank" rel="noreferrer">front end repo</a> | <a class="projects-info" href="https://github.com/tylerdance/SEI-project4-backend" target="_blank" rel="noreferrer">back end repo</a></div>
+                            <div></div>
 
+                        </div>
+                    </div>
+                    <div>
+                        <img className="ss" src="http://res.cloudinary.com/dok4pz3i3/image/upload/v1612022377/Screen_Shot_2021-01-30_at_10.58.09_AM_xni1d0.png" alt="" />
                     </div>
                 </div>
-                <div>
-                    <img className="ss" src="http://res.cloudinary.com/dok4pz3i3/image/upload/v1612022377/Screen_Shot_2021-01-30_at_10.58.09_AM_xni1d0.png" alt="" />
-                </div>
-            </div>
 
-            <div class="tab-sensei-div">
-                <div id="tabsensei-sub-div">
-                    <h3><a class="recents-header" href="http://tabsensei.herokuapp.com/" target="_blank" rel="noreferrer">Tab Sensei</a></h3>
-                    {/* <div>
-                        <iframe class="vid" src="https://www.youtube.com/embed/Fa00WE9fpC4?list=PLfTlLh3QA5Fk8NAIQZlEmxG_mL9GsJbYK" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div> */}
-                    <p class="description">Guitar tab search engine and social platform. Users can search for tabs, comment, save tabs to their profile, and play along to beats from the beat bank at any tempo.</p>
-                    {/* <br /> */}
-                    <p class="tech-stack"><strong>React.js, MongoDB, Express, Node.js</strong></p>
-                    <div class="links-div">
-                        <div><a class="projects-info" href="http://tabsensei.herokuapp.com/" target="_blank" rel="noreferrer">live site</a></div>
-                        <div><a class="projects-info" href="https://github.com/tylerdance/tab-sensei-frontend" target="_blank" rel="noreferrer">front end repo</a> | <a class="projects-info" href="https://github.com/tylerdance/tab-sensei-backend" target="_blank" rel="noreferrer">back end repo</a></div>
-                        <div></div>
+                <div class="tab-sensei-div">
+                    <div id="tabsensei-sub-div">
+                        <h3><a class="recents-header" href="http://tabsensei.herokuapp.com/" target="_blank" rel="noreferrer">Tab Sensei</a></h3>
+                        {/* <div>
+                            <iframe class="vid" src="https://www.youtube.com/embed/Fa00WE9fpC4?list=PLfTlLh3QA5Fk8NAIQZlEmxG_mL9GsJbYK" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div> */}
+                        <p class="description">Guitar tab search engine and social platform. Users can search for tabs, comment, save tabs to their profile, and play along to beats from the beat bank at any tempo.</p>
+                        {/* <br /> */}
+                        <p class="tech-stack"><strong>React.js, MongoDB, Express, Node.js</strong></p>
+                        <div class="links-div">
+                            <div><a class="projects-info" href="http://tabsensei.herokuapp.com/" target="_blank" rel="noreferrer">live site</a></div>
+                            <div><a class="projects-info" href="https://github.com/tylerdance/tab-sensei-frontend" target="_blank" rel="noreferrer">front end repo</a> | <a class="projects-info" href="https://github.com/tylerdance/tab-sensei-backend" target="_blank" rel="noreferrer">back end repo</a></div>
+                            <div></div>
 
+                        </div>
+                    </div>
+                    <div>
+                        <img className="ss" src="http://res.cloudinary.com/dok4pz3i3/image/upload/v1612022956/Screen_Shot_2021-01-30_at_11.08.56_AM_sjxo4y.png" alt="" />
                     </div>
                 </div>
-                <div>
-                    <img className="ss" src="http://res.cloudinary.com/dok4pz3i3/image/upload/v1612022956/Screen_Shot_2021-01-30_at_11.08.56_AM_sjxo4y.png" alt="" />
-                </div>
-            </div>
 
             {/* <div class="trail-hunter-div">
                 <div>
