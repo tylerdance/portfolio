125175e2f00daa9586f58292d3a6f33d0914cd9a
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-03-13 11:24:55 -0500

    resume update

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ function Home() {
                     <h1 id="my-name-home">Tyler Dance</h1>
                     {/* <img id="prof-pic" src="http://res.cloudinary.com/dok4pz3i3/image/upload/v1612141644/headshot_yoackc.jpg" alt="" /> */}
                     <p id="bio">Full stack software developer & audio engineer</p>
-                    
+
                     <div id="my-links">
-                        <p><a id="github" href="https://github.com/tylerdance" target="_blank" rel="noreferrer">Github</a> | <a id="linkedin" href="https://www.linkedin.com/in/tylerdance/" target="_blank" rel="noreferrer">LinkedIn</a><span> | <a href="https://docdro.id/z4aFqeC" target="_blank" rel="noreferrer">Resume</a></span></p>
+                        <p><a id="github" href="https://github.com/tylerdance" target="_blank" rel="noreferrer">Github</a> | <a id="linkedin" href="https://www.linkedin.com/in/tylerdance/" target="_blank" rel="noreferrer">LinkedIn</a><span> | <a href="https://docdro.id/4Bk5fnC" target="_blank" rel="noreferrer">Resume</a></span></p>
 
                     </div>
                     {/* <p id="contact">tylerdance12@gmail.com</p> */}
